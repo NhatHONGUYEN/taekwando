0dc2e5b256a4aaba7f42b62650ea919076d743c8
Nhat-Quan HO NGUYEN <hnnhat67@gmail.com>
2026-03-03 15:30:42 +0100

    add package

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const cors = require("cors");
+const todosRouter = require("./routes/todos");
+const errorHandler = require("./middlewares/errorHandler");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./swagger");
+const helmet = require("helmet");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use(helmet());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get("/health", (req, res) => {
+  res.json({ ok: true });
+});
+
+app.use("/todos", todosRouter);
+
+// Le gestionnaire d'erreurs doit être après toutes les routes
+app.use(errorHandler);
 
 module.exports = app;
