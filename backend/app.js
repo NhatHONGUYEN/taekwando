bb4129842518525f4c0b8d8aed02baec7ba61c04
Nhat-Quan HO NGUYEN <hnnhat67@gmail.com>
2026-03-04 08:56:55 +0100

    errorHandler add

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const todosRouter = require("./routes/todos");
 const errorHandler = require("./middlewares/errorHandler");
-const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("./swagger");
 const helmet = require("helmet");
 
 const app = express();
@@ -11,14 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(helmet());
 
 app.get("/health", (req, res) => {
   res.json({ ok: true });
 });
-
-app.use("/todos", todosRouter);
 
 // Le gestionnaire d'erreurs doit être après toutes les routes
 app.use(errorHandler);
